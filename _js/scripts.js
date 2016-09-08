11bdb1d837b8febd4bf4e681fcbd992a977edafe
Fallenstedt <a.fallenstedt@gmail.com>
2016-09-07 17:32:17 -0700

    modify structure of nav s

--- _js/scripts.js
+++ _js/scripts.js
@@ -82,14 +82,20 @@ var Welcome = (function () {
   };
 })();
 
-function toggleNav() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+
+var Nav = (function(){
+  var toggle = function() {
+      var x = document.getElementById("myTopNav");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
+  }
+  return {
+    toggle: toggle
+  }
+})();
 
 
 $(document).ready(function(){
