a5911a352c98fa8726442990e4d4b58f0ab9d5ff
Fallenstedt <a.fallenstedt@gmail.com>
2016-09-12 08:17:21 -0700

    modify photography.js

--- _js/photography.js
+++ _js/photography.js
@@ -3,17 +3,25 @@
 
 //scripts to be used when a page is loaded with the 'standard' layout
 var Photography = (function(){
+
+  var id = function(element){
+    return document.getElementById(element);
+  }
+
   //obtain a list of images with a given search term.
   var obtainImages = function(search) {
+    var backgroundImages = new Array();
     $.ajax({
       url: "../images",
       success: function(data){
         $(data).find("a:contains(" + search + ")").each(function(){
           // will loop through
-          $('<p></p>').html(this).appendTo('#photographer-poster')
+          backgroundImages.push(this.innerHTML)
+          // $('<p></p>').html(this).appendTo('#photographer-poster')
         });
       }
     });
+    return backgroundImages;
   }
 
   //Before DOM is unloaded, apply a class called "animate-out".
@@ -24,12 +32,16 @@ var Photography = (function(){
     });
   }
 
-  // var backgroundImageCarousel = function() {
-  //   //take an array of image names and cycle through them for the first section on photography
-  // }
+  var backgroundImageCarousel = function(array) {
+    //take an array of image names and cycle through them for the first section on photography
+    alert(array)
+    id('photographer-poster').style.background = 'url(' + array[0] + ')';
+  }
 
   return {
+    id: id,
     obtainImages: obtainImages,
+    backgroundImageCarousel: backgroundImageCarousel,
     fadeOutBeforeUnload: fadeOutBeforeUnload,
   }
 })();
@@ -37,5 +49,5 @@ var Photography = (function(){
 
 $(document).ready(function(){
   Photography.fadeOutBeforeUnload();
-  Photography.obtainImages('cat');
+  var myBgImgs = Photography.obtainImages('cat');
 });
