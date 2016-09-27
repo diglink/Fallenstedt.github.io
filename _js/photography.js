393a23302329d663698c056234636d5fc957338e
Fallenstedt <a.fallenstedt@gmail.com>
2016-09-26 21:21:44 -0700

    modify photgoraphy.html bebehavior and content

--- _js/photography.js
+++ _js/photography.js
@@ -4,54 +4,22 @@
   //Private
   var _photo = {};
   var IMAGES = [
-    '../images/nature-1.jpg',
-    '../images/nature-2.jpg',
-    '../images/nature-3.jpg',
-    '../images/nature-4.jpg'
+    '../images/nature/nature-1.jpg',
+    '../images/nature/nature-2.jpg',
+    '../images/nature/nature-3.jpg',
+    '../images/nature/nature-4.jpg'
   ]
 
   //logic
   function backgroundImageCarousel() {
-    console.log(IMAGES);
   }
 
 
   //Public
   App.Photography = {
     init: function(){
-      _photo.backgroundImageCarousel = backgroundImageCarousel();
+
     }
   }
 
 })(jQuery, window.App = window.App || {});
-
-
-
-
-
-
-
-// var images = [
-//
-//   'path/to/image1.jpg',
-//   'path/to/image2.jpg',
-//   'path/to/image3.jpg'
-//
-// ];
-//
-// var index = 0;
-//
-// setInterval(change_up, 1000);
-//
-// function change_up(){
-//
-//   index = (index + 1 < images.length) ? index + 1 : 0;
-//
-//   $('.block').fadeOut(300, function(){
-//
-//     $(this).css('background-image', 'url('+ images[index] + ')')
-//
-//     $(this).fadeIn(300);
-//
-//   });
-// }
