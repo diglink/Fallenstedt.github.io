f711dd1dbc049a06330361451bc4d2454327dede
Fallenstedt <a.fallenstedt@gmail.com>
2016-10-17 21:53:48 -0700

    revert

---
+++ _js/portfolio.js
@@ -0,0 +1,34 @@
+
+(function($, App){
+
+  //Private
+  var _portfolio = {};
+
+  //logic
+  // function createCarousel() {
+  //   console.log('hello')
+  //   $('.slider-for').slick({
+  //    slidesToShow: 1,
+  //    slidesToScroll: 1,
+  //    arrows: false,
+  //    fade: true,
+  //    asNavFor: '.slider-nav'
+  //   });
+  //   $('.slider-nav').slick({
+  //    slidesToShow: 3,
+  //    slidesToScroll: 1,
+  //    asNavFor: '.slider-for',
+  //    dots: true,
+  //    centerMode: true,
+  //    focusOnSelect: true
+  //   });
+  // }
+
+  //Public
+  App.Photography = {
+    init: function(){
+      // _portfolio.slider = createCarousel();
+    }
+  }
+
+})(jQuery, window.App = window.App || {});
