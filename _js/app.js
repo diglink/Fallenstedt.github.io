bae20edccf7dc9fd2e448fc7a98ddbe32bb51725
Fallenstedt <a.fallenstedt@gmail.com>
2016-11-04 21:23:21 -0700

    save progress on code section

--- _js/app.js
+++ _js/app.js
@@ -1,12 +1,14 @@
 (function($, App){
   //Private
+  var _app = {};
+  
 
   //Public
   App.State = {
     init: function(){
+      App.Helpers.showMe();
       App.Index.init();
       App.Nav.init();
-      App.Photography.init();
       if (/portfolios/.test(window.location.href)) {
         App.Portfolio.init();
       }
